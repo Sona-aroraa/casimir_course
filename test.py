29a2531c824ac85fff18980a4d542d9136403a99
Sona <s.arora@tudelft.nl>
2021-01-25 14:18:22 +0100

    Changed test file

--- test.py
+++ test.py
@@ -3,13 +3,13 @@ import numpy as np
 print("Hello world")
 
 def circumference(radius):
-'''calculating the circumference of a circle
-'''
+	'''calculating the circumference of a circle
+	'''
 	return  2*np.pi*radius
 
 def surface(radius):
-'''calculating surface of the circle
-'''
+	'''calculating surface of the circle
+	'''
 	return np.pi*radius**2
 
 
