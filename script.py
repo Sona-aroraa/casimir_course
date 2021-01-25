f610eddc8431276d4b93f46116bafc1d0fbd73e2
Sona <s.arora@tudelft.nl>
2021-01-25 15:21:34 +0100

    Sona entered an important fact

--- script.py
+++ script.py
@@ -1,4 +1,9 @@
 import test
 
-circum = test.circumference(radius)
-print("circumference: ", circum)
+circum = test.circumference(4)
+#print("circumference: ", circum)
+
+h =  input("Enter name?")
+s = input("Enter name 2?")
+
+print(h ," and ",s, " are awesome!")
